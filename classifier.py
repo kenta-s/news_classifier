475a8f645068ff799c3fc4f6f22a69ed58e887cb
kenta-s <knt01222@gmail.com>
2017-06-18 10:04:06 +0900

    Refactor whole classifier

--- classifier.py
+++ classifier.py
@@ -28,15 +28,6 @@ import json
 # for debug
 from IPython import embed
 from IPython.terminal.embed import InteractiveShellEmbed
-# embed()
-
-# import pandas as pd
-# usage of pandas is below
-# foo = pd.read_csv('sample_news/yahoonews.csv')
-# foo['content'] # this should be a column name
-
-# df = pd.read_csv('sample_news/yahoonews.csv')
-# news_list = df.values
 
 def extract_words(text):
     node = mecab.parseToNode(text)
@@ -52,76 +43,61 @@ f = open('sample_news/yahoo_news.json', 'r')
 news_list = json.load(f)
 f.close()
 dictionary_name = 'words.txt'
-dictionary = corpora.Dictionary.load_from_text(dictionary_name)
+words = []
+for key in news_list:
+    news = news_list[key]
+    text = news['content']
+    words.append(extract_words(text))
+
+dictionary = corpora.Dictionary(words)
+dictionary.save_as_text(dictionary_name)
+
+x_list = []
+y_list = []
 for key in news_list:
     news = news_list[key]
     text = news['content']
     words = extract_words(text)
-    new_dictionary = corpora.Dictionary([words])
-    dictionary.merge_with(new_dictionary)
-    dictionary.save_as_text(dictionary_name)
+    vec = dictionary.doc2bow(words)
+    dense = list(matutils.corpus2dense([vec], num_terms=len(dictionary)).T[0])
+    x_list.append(dense)
+    y_list.append(int(news['label']))
 
-for i in range(100):
-    for key in news_list:
-        news = news_list[key]
-        text = news['content']
-        words = extract_words(text)
+X = np.array(x_list).astype(np.float32)
+Y = np.array(y_list).astype(np.int32)
+N = len(X)
+Y2 = np.zeros(3 * N).reshape(N, 3).astype(np.float32)
+for i in range(N):
+    Y2[i, Y[i]] = 1.0
 
-        y = np.zeros(3).astype(np.float32)
-        index = int(news['label'])
-        y[index] = 1.0
-        y = Variable(y).reshape(1, 3)
-        vec = dictionary.doc2bow(words)
-        dense = list(matutils.corpus2dense([vec], num_terms=len(dictionary)).T[0])
-        # cut_dense = dense[0:10]
+index = np.arange(N)
+xtrain = X[index[index % 2 != 0]]
+ytrain = Y2[index[index % 2 != 0]]
+xtest = X[index[index % 2 == 0]]
+yans = Y[index[index % 2 == 0]]
 
-        # x = Variable(np.array(cut_dense).astype(np.float32).reshape(1, 1853))
-        x = Variable(np.array(dense).astype(np.float32).reshape(1, 1853))
-        # sports: 2
-        # y = [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] # refactor this shit
-        # y = y[0:8]
-        # y = Variable(np.array(y).astype(np.float32).reshape(1, 8))
+n = 98
+bs = 25
+for j in range(5000):
+    sffindx = np.random.permutation(n)
+    for i in range(0, n, bs):
+        idx = sffindx[i:(i+bs) if (i+bs) < n else n]
+        x = Variable(xtrain[idx])
+        y = Variable(ytrain[idx])
         model.zerograds()
         loss = model(x, y)
         loss.backward()
         optimizer.update()
 
-text = "【森下悠里が結婚 昨年から交際】タレントの森下悠里がインスタグラムで、昨年末から交際していた男性と6月8日に結婚したことを報告。10年来の友人だという。"
-words = extract_words(text)
-
-# embed()
-
-# # save
-# dictionary.save_as_text('hoge.txt')
-
-# # load
-# dictionary = corpora.Dictionay.load_from_text('hoge.txt')
-
-vec = dictionary.doc2bow(words)
-dense = list(matutils.corpus2dense([vec], num_terms=len(dictionary)).T[0])
-# cut_dense = dense[0:10]
-
-# xt = Variable(np.array(cut_dense).astype(np.float32).reshape(1, 1853))
-xt = Variable(np.array(dense).astype(np.float32).reshape(1, 1853))
+xt = Variable(xtest)
 yt = model.fwd(xt)
 ans = yt.data
+nrow, ncol = ans.shape
+ok = 0
 
-print(ans)
-# embed()
-# nrow, ncol = ans.shape
+for i in range(nrow):
+    cls = np.argmax(ans[i,:])
+    if cls == yans[i]:
+      ok += 1
 
-# for i in range(100):
-#     x = Variable(np.array(cut_dense).astype(np.float32).reshape(1, 10))
-#     # sports: 2
-#     y = [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0] # refactor this shit
-#     y = y[0:8]
-#     y = Variable(np.array(y).astype(np.float32).reshape(1, 8))
-#     model.zerograds()
-#     loss = model(x, y)
-#     loss.backward()
-#     optimizer.update()
-
-# from IPython import embed
-# from IPython.terminal.embed import InteractiveShellEmbed
-#
-# embed()
+print(ok, "/", nrow, " = ", (ok * 1.0)/nrow)

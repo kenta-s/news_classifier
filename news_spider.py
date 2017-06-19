adb0c001eeaab29c0ef22b528b31e984b4e077e4
kenta-s <knt01222@gmail.com>
2017-06-20 08:18:11 +0900

    Add source to json file

--- news_spider.py
+++ news_spider.py
@@ -20,18 +20,27 @@ class NewsSpider(Spider):
     }
 
     def parse(self, response):
+        if 'YahooNews' in response.url:
+            self.yahoo_news(response)
+        else:
+            return False
+            # yield scrapy.Request(tweet, callback=self.parse_item)
+
+    def yahoo_news(self, response):
+        source = 'YahooNews'
         file_name = 'unlabelled_data/yahoo_news.json'
         f = open(file_name, 'r')
         tweets = json.load(f)
+        f.close()
         tweet_list = 'li.stream-item'
         for tweet_item in response.css(tweet_list):
             tweet_id = tweet_item.css('li::attr(data-item-id)').extract()[0]
             tweet_text = tweet_item.css('p.tweet-text::text').extract()[0]
-            tweets[tweet_id] = {'content': tweet_text, 'label': None}
+            tweets[tweet_id] = {'source': source, 'content': tweet_text, 'label': None}
         news_json = codecs.open(file_name, 'w', 'utf-8')
         json.dump(tweets, news_json, ensure_ascii=False)
         news_json.close()
-            # yield scrapy.Request(tweet, callback=self.parse_item)
+
 
     def parse_item(self, response):
         return True

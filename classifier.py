e9c571d569ad652764171e46256df1448994c99e
kenta-s <knt01222@gmail.com>
2017-06-23 09:47:13 +0900

    Add TwitterNewsJP

--- classifier.py
+++ classifier.py
@@ -39,7 +39,11 @@ def extract_words(text):
 def get_news_list():
     f = open('sample_news/news.json', 'r')
     news_list = json.load(f)
-    news_list = [news_list['YahooNews'], news_list['news_line_me'], news_list['nhk_news']]
+    news_list = [
+        news_list['YahooNews'],
+        news_list['news_line_me'],
+        news_list['TwitterNewsJP'],
+        news_list['nhk_news']]
     f.close()
 
     return news_list

a5d016ccc8a201fac6d4bf10ac4713dba3107510
kenta-s <knt01222@gmail.com>
2017-06-25 10:15:29 +0900

    Make callable from another path

--- news_classifier.py
+++ news_classifier.py
@@ -1,4 +1,5 @@
-import sys
+import sys, os
+import json
 import numpy as np
 from chainer import Variable, optimizers, serializers
 from news_chain import NewsChain
@@ -30,13 +31,23 @@ def convert_text_into_variable(dictionary, text):
     dense = convert_text_into_dense(dictionary, text)
     return Variable(np.array([dense]))
 
-dictionary = corpora.Dictionary.load_from_text('trained/words.txt')
+base_path = os.path.dirname(os.path.abspath(__file__))
+dic_file = os.path.normpath(os.path.join(base_path, 'trained/words.txt'))
+npz_file = os.path.normpath(os.path.join(base_path, 'trained/news_classifier.npz'))
+category_file = os.path.normpath(os.path.join(base_path, 'categories.json'))
+
+dictionary = corpora.Dictionary.load_from_text(dic_file)
 input_length = len(dictionary)
 model = NewsChain(input_length)
-serializers.load_npz('trained/news_classifier.npz', model)
+serializers.load_npz(npz_file, model)
 
 text = sys.argv[1]
 x = convert_text_into_variable(dictionary, text)
 loss = model.fwd(x)
 
-print(loss)
+f = open(category_file, 'r')
+categories = json.load(f)
+f.close()
+idx = str(np.argmax(loss.data))
+
+print(categories[idx])
